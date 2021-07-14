b9057790cb7a61d200ac7974394dcef7c4e2eebd
endhel <endheltog@gmail.com>
2021-07-14 11:07:56 -0300

    changed folder structure

--- store/admin/routes.py
+++ store/admin/routes.py
@@ -5,3 +5,7 @@ from store import app, db
 @app.route('/')
 def home():
     return "Seja bem-vindo ao InfoStore"
+
+@app.route('/register')
+def register():
+    return render_template('admin/register.html', title='Register user')
