fe14997148e0aecfd88ae5d46dde9b0013587e41
endhel <endheltog@gmail.com>
2021-08-20 10:00:13 -0300

    Created shopping cart page

--- store/shopping_cart/carts.py
+++ store/shopping_cart/carts.py
@@ -19,7 +19,7 @@ def addCart():
 
         if product_id and quantity and colors and request.method=="POST":
             DictItems = {product_id:{'Name':product.name, 'Price':float(product.price), 'Discount':product.discount, 
-            'Color':colors, 'Quantity':quantity, 'Image':product.image_1,}}
+            'Color':colors, 'Quantity':quantity, 'Image':product.image_1}}
             if 'StoreinCart' in session:
                 print(session['StoreinCart'])
                 if product_id in session['StoreinCart']:
@@ -35,3 +35,19 @@ def addCart():
         print(e)
     finally: 
         return redirect(request.referrer)
+
+@app.route('/carts')
+def getCart():
+    if 'StoreinCart' not in session:
+        return redirect(request.referrer)
+    
+    subtotal = 0
+    total = 0
+
+    for key, product in session['StoreinCart'].items():
+        discount = (product['Discount'] / 100) * float(product['Price'])
+        subtotal += float(product['Price']) * int(product['Quantity'])
+        subtotal -= discount
+        tax = ("%.2f" %(.06 * float(subtotal)))
+        total = float("%.2f" %(1.06 * subtotal))
+    return render_template('products/carts.html', tax=tax, total=total)
